7029be33d3eba8b10b0506534b37612172d1cf5e
aldmCh <116429716+aldmCh@users.noreply.github.com>
2022-10-22 12:09:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main()
+        {
+            byte n = 4;
+            for (byte i = 1; i <= n; ++i)
+            {
+                for (byte j = 1; j <= i; ++j)
+                {
+                    Console.Write("{0,9}", i);
+                }
+                Console.WriteLine();
+                for (byte j = 1; j <= i; ++j)
+                {
+                    Console.Write("{0,9}", i + 5);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
